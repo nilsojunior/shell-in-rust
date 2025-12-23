894ddda6683a60998902f8fc56f8d63e927f3efb
Nilso Júnior <162613094+Nilsojunior@users.noreply.github.com>
2025-12-22 22:56:40 -0300

    feat: user input

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    println!("{}: command not found", input.trim());
 }
